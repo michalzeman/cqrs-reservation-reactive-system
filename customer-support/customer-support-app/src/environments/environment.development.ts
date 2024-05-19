788a14fe55a438dfb203dbae06e7ba3677ab2ac9
Michal Zeman <zeman.michal@yahoo.com>
2024-05-19 14:02:07 +0200

    Extract webSocket url into the environment configuration

---
+++ customer-support/customer-support-app/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  wsApiUrl: 'ws://localhost:8080'
+};
---
+++ customer-support/customer-support-app/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  wsApiUrl: 'ws://localhost:8080'
+};
